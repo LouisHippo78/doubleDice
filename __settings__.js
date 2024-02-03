a70af3250453287137f283778eb523050ec93776
LouisHippo78 <158695920+LouisHippo78@users.noreply.github.com>
2024-02-03 15:55:40 +0900

    doubleDice

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1917257.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 160100458, 160100407, 160100453, 160100461, 160100399, 160100427, 160100476, 160100397, 160100481, 160100429, 160100439, 160100430, 160100436, 160100459, 160100474, 160108784, 160274729 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
